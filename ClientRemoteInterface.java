735fa33e40dd3c4f91d740006b0f89e95dde01db
niccolomarastoni <eightnine@gmail.com>
2013-05-25 13:16:16 +0200

    Create ClientRemoteInterface.java

---
+++ ClientRemoteInterface.java
@@ -0,0 +1,11 @@
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface ClientRemoteInterface extends Remote {
+  public void getOpponent() throws MalformedURLException, RemoteException, NotBoundException;
+	public void setBallSpeed(double dx, double dy) throws RemoteException;
+	public double[] getUpdate() throws RemoteException;
+}

5c3e16106f78f97e6468a3cea77b02245f6acdee
Andrei Munteanu <mantundrei@gmail.com>
2013-06-01 09:05:05 +0200

    0.3

--- ClientRemoteInterface.java
+++ ClientRemoteInterface.java
@@ -5,7 +5,9 @@ import java.rmi.RemoteException;
 
 
 public interface ClientRemoteInterface extends Remote {
-  public void getOpponent() throws MalformedURLException, RemoteException, NotBoundException;
-	public void setBallSpeed(double dx, double dy) throws RemoteException;
-	public double[] Update(double baseX, double baseY) throws RemoteException;
+	public void getOpponent() throws MalformedURLException, RemoteException, NotBoundException;
+	public void setBall(double dx, double dy, double x, double y) throws RemoteException;
+	public void Update(double baseX, double baseY) throws RemoteException;
+	public void updateScore(int p1Score, int p2Score) throws RemoteException;
+	public void startGame() throws RemoteException;
 }

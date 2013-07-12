0a5d50c1b6e01d67b3a698ce78a89bd382cbf24c
Andrei Munteanu <mantundrei@gmail.com>
2013-07-12 13:45:24 +0200

    nicc l'ha dimenticato

--- myKeyTool.sh
+++ myKeyTool.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #	keytool -genkey -keyalg RSA -keystore authServer.keystore -dname "CN=authServer, OU=Bar, O=Foo, L=Some, ST=Where, C=UN" -storepass server -keypass authServer
 #	keytool -genkey -keyalg RSA -keystore loginClient.keystore -dname "CN=loginClient, OU=Bar, O=Foo, L=Some, ST=Where, C=UN" -storepass loginClient -keypass loginClient
 #	keytool -export -rfc -keystore authServer.keystore -alias pongkey -file authServer.public-key -storepass authServer
@@ -6,9 +5,6 @@
 #	keytool -import -trustcacerts -alias loginClient -keystore authServer.keystore -file loginClient.public-key -storepass authServer
 #	keytool -import -trustcacerts -alias authServer -keystore loginClient.keystore -file authServer.public-key -storepass loginClient
 	keytool -genkey -keyalg RSA -keystore server.keystore -dname "CN=Server, OU=Bar, O=Foo, L=Some, ST=Where, C=UN" -storepass server -keypass server
-=======
-  keytool -genkey -keyalg RSA -keystore server.keystore -dname "CN=Server, OU=Bar, O=Foo, L=Some, ST=Where, C=UN" -storepass server -keypass server
->>>>>>> ad15281f9e80ae41f28e17530f739e72e9ebf2bf
 	keytool -genkey -keyalg RSA -keystore client.keystore -dname "CN=Client, OU=Bar, O=Foo, L=Some, ST=Where, C=UN" -storepass client -keypass client
 	keytool -export -rfc -keystore server.keystore -alias mykey -file server.public-key -storepass server
 	keytool -export -rfc -keystore client.keystore -alias mykey -file client.public-key -storepass client

9165a98c5128ace48fc262e64c175de163c1d51b
niccolomarastoni <eightnine@gmail.com>
2013-07-17 18:10:51 +0200

    Update ServerPong.java

--- ServerPong.java
+++ ServerPong.java
@@ -1,4 +1,5 @@
 package tetraPong;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.rmi.MarshalledObject;
 import java.rmi.NoSuchObjectException;
@@ -11,11 +12,9 @@ import java.rmi.server.Unreferenced;
 
 public class ServerPong extends Activatable 
 implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
-	String ip = "//157.27.241.199";
 	private Pong pong;
 	private PlayerInterface opponent = null;
 	private PlayerToMainInterface mainRef = null;
-	private boolean myTurn = false;
 	private int gameID;
 	private int id;
 	private String user;
@@ -30,15 +29,19 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 	public void activate(String user) throws RemoteException {
 		this.user = user;
 		pong = new Pong(this);
+		System.out.println(this +" Activated ");
 		if((gameID = mainRef.getMatch(this)) == -1)
 			System.out.println("Waiting! :D");
+		else if(gameID == -2){
+			System.out.println("User "+user+ " banned popup");
+			pong.f.dispatchEvent(new WindowEvent(pong.f, WindowEvent.WINDOW_CLOSING));
+		}
 		else{
 			id = 0;
 			opponent = mainRef.getOpponent(gameID,1);
 		}
-		
-		System.out.println(this +" Activated ");
-			
+
+
 	}
 	@Override
 	public void setBall(double dx, double dy, double x, double y) throws RemoteException{
@@ -70,47 +73,63 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 	}
 
 	public void updateOppentScore() {
+		boolean error = false;
 		try {
 			opponent.updateScore(pong.p2Score, pong.p1Score);
 			pong.starting = true;
 			opponent.setBall(-pong.dx,-pong.dy, 250, 250);
 		} catch (RemoteException e) {
-			try {
+			error = true;
+		}
+		try {
+			if(error)
 				mainRef.errorSignal(gameID,id);
-			} catch (RemoteException e1) {
-				e1.printStackTrace();
-			}
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
 		}
 	}
 
 	public void updateOpponentBase(){
+		boolean error = false;
 		try {
 			opponent.Update(454 - pong.lowBase.x, 455 - pong.leftBase.y);
 		} catch (RemoteException e) {
-			try {
+			error = true;
+		}
+		try {
+			if(error)
 				mainRef.errorSignal(gameID,id);
-			} catch (RemoteException e1) {
-				e1.printStackTrace();
-			}
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
 		}
 	}
 	public void updateOpponentBallSpeed(){
+		boolean error = false;
 		try {
 			opponent.setBall(-pong.dx,-pong.dy, 480- pong.ball.x, 480 - pong.ball.y);
 		} catch (RemoteException e) {
-			try {
+			error = true;
+		}
+		try {
+			if(error)
 				mainRef.errorSignal(gameID,id);
-			} catch (RemoteException e1) {
-				e1.printStackTrace();
-			}
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
 		}
 	}
 
 	@Override
-	public void updateScore(int p1Score, int p2Score) throws RemoteException {
-		pong.starting = true;
+	public void updateScore(int p1Score, int p2Score) throws RemoteException {	
 		pong.p1Score = p1Score;
 		pong.p2Score = p2Score;
+		if(p1Score == 5 || p2Score == 5){
+			pong.starting = false;
+			pong.running = false;
+			pong.gameEnded = true;
+			pong.showVictory("YOU WON");
+		}
+		else
+			pong.starting = true;
 	}
 
 	@Override
@@ -119,12 +138,21 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 		pong.highBase.x = baseX;
 	}
 
+
+	@Override
+	public void stopGame() throws RemoteException {
+		pong.gameEnded = true;
+		pong.running = false;
+		pong.starting = false;
+		pong.showVictory("ADMIN STOP");
+	}
+
 	@Override
 	public void gameReady(int gameID) throws RemoteException {
 		opponent = mainRef.getOpponent(gameID,0);	
 		this.gameID = gameID;
 		id =1;
-		System.out.println("Hello received player id = "+id+opponent.getUser());
+		System.out.println("Hello received player id = "+opponent.getUser());
 		init();
 
 	}
@@ -160,6 +188,7 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 			e.printStackTrace();
 		}
 		System.out.println("Successfully called unregister");
+		pong.setVisible(false);
 		pong.deleteProxyRef();
 		pong.setEnabled(false);
 		pong = null;
@@ -168,18 +197,18 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 
 	@Override
 	public void opponentLeft() throws RemoteException {
-		//ferma il gioco
-		//apri finestra di dialogo
-		System.out.println("Vuoid giocare?");
-		
+		pong.starting = false;
+		pong.running = false;
+		if(!pong.gameEnded)
+			pong.showVictory("OPPONENT LEFT");
+
 	}
-	
+
 	@Override
 	public String getScore() throws RemoteException {
-		
-		return ""+pong.p1Score+" - " + pong.p2Score;
+		return "" + pong.p1Score + " - " + pong.p2Score;
 	}
-	
+
 	@Override
 	public String toString(){
 		return user;
@@ -190,6 +219,9 @@ implements PlayerInterface ,TetraPongProxy,MainToPlayerInterface,Unreferenced{
 		return user;
 	}
 
-	
+	@Override
+	public int ping() throws RemoteException {
+		return 1;		
+	}
 
 }

a88450e682c98ae9faaddb946f37392eb1fed41a
niccolomarastoni <eightnine@gmail.com>
2013-07-17 18:12:55 +0200

    Update MainToPlayerInterface.java

--- MainToPlayerInterface.java
+++ MainToPlayerInterface.java
@@ -7,4 +7,6 @@ public interface MainToPlayerInterface extends Remote{
 	public void gameReady(int gameID) throws RemoteException;
 	public void opponentLeft() throws RemoteException;
 	public String getScore() throws RemoteException;
+	public void stopGame() throws RemoteException;
+	public int ping() throws RemoteException;
 }
